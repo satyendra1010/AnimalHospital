80b63a9cb2e3ae935bda8775b2da47966f5f32cb
satyendra1010 <satyendra1010@gmail.com>
2023-08-22 15:14:50 -0700

    Initialized the project.

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+import styles from "../styles/home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div className={styles.upperLeft}>
+        <p>{`<Back`}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
